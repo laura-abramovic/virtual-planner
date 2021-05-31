68f64942ec23ded67077743b6a1539f470385263
FER\la51450 <abramovic.laura@gmail.com>
2021-05-31 20:29:12 +0200

    Added changing fragments on click

--- app/src/main/java/com/android/virtualplanner/activities/visuals/MainScreenActivity.kt
+++ app/src/main/java/com/android/virtualplanner/activities/visuals/MainScreenActivity.kt
@@ -2,8 +2,12 @@ package com.android.virtualplanner.activities.visuals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.android.virtualplanner.R
+import com.android.virtualplanner.fragments.CalendarFragment
 import com.android.virtualplanner.fragments.HomeFragment
+import com.android.virtualplanner.fragments.NotesFragment
+import com.android.virtualplanner.fragments.TodoFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainScreenActivity : AppCompatActivity() {
@@ -13,30 +17,38 @@ class MainScreenActivity : AppCompatActivity() {
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
+        val fragment = HomeFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
+                .commit()
+
         bottomNav.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navHomeId -> {
-//                    val fragment = HomeFragment
-//                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-//                        .commit()
+                    val fragment = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
+                        .commit()
                     true
                 }
                 R.id.navCalendarId -> {
+                    val fragment = CalendarFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
+                            .commit()
                     true
                 }
                 R.id.navTodoId -> {
+                    val fragment = TodoFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
+                            .commit()
                     true
                 }
                 R.id.navNotesId -> {
+                    val fragment = NotesFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
+                            .commit()
                     true
                 }
             }
             false
         }
     }
-
-//    private val navListener =
-//        BottomNavigationView.OnNavigationItemSelectedListener {
-//            fun onNavigationItemSelected
-//        }
 }
