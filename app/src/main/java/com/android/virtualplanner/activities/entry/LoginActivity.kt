8b9de048ebb5604b1efd31639035bf0b50525ae5
FER\la51450 <abramovic.laura@gmail.com>
2021-06-04 22:04:19 +0200

    Added user login

--- app/src/main/java/com/android/virtualplanner/activities/entry/LoginActivity.kt
+++ app/src/main/java/com/android/virtualplanner/activities/entry/LoginActivity.kt
@@ -9,44 +9,40 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.android.virtualplanner.R
+import com.android.virtualplanner.dao.UserDao
 import com.android.virtualplanner.database.UserDatabase
+import com.android.virtualplanner.entities.User
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.mindrot.jbcrypt.BCrypt
 
 class LoginActivity : AppCompatActivity() {
-    private val dao = UserDatabase.getInstance(this).userDao
+    private var inputUsername: String = ""
+    private lateinit var dao: UserDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         hideActionBar()
         setContentView(R.layout.activity_login)
 
-        val forgotPassLink = findViewById<TextView>(R.id.loginForgotPasswordTextId)
         val singInButton = findViewById<Button>(R.id.loginSingInButtonId)
         val singUpLink = findViewById<TextView>(R.id.loginBottomLinkId)
 
-        //val dao = UserDatabase.getInstance(this).userDao
-
-        forgotPassLink.setOnClickListener {
-            Toast.makeText(this, "We will send you an email once we implement that", Toast.LENGTH_SHORT).show()
-        }
+        dao = UserDatabase.getInstance(this).userDao
 
         singInButton.setOnClickListener {
-            val inputUsername = findViewById<TextInputEditText>(R.id.loginUsernameEditTextId).text.toString()
-            var exists = false
+            var users = runBlocking { dao.getUsers() } // get users from database
 
-            lifecycleScope.launch {
-                isCorrectInput()
-
-            }
-
-            if (!exists) {
-                Toast.makeText(this, "Username or password invalid", Toast.LENGTH_SHORT).show()
+            var error = findViewById<TextView>(R.id.loginErrorId)
+            if (!isCorrectInput(users)) {
+                error.setText(R.string.login_screen_error)
             } else {
+                error.setText(R.string.empty_string)
                 Toast.makeText(this, "You are now singed in", Toast.LENGTH_SHORT).show()
             }
 
-            //Toast.makeText(this, "You are now singed in", Toast.LENGTH_SHORT).show()
+
         }
 
         singUpLink.setOnClickListener {
@@ -61,26 +57,17 @@ class LoginActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {}
     }
 
-    private suspend fun isCorrectInput(): Boolean {
-        val inputUsername = findViewById<TextInputEditText>(R.id.loginUsernameEditTextId).text.toString()
-        val users = dao.getUsers()
+    private  fun isCorrectInput(users: List<User>): Boolean {
+        inputUsername = findViewById<TextInputEditText>(R.id.loginUsernameEditTextId).text.toString()
 
         for (user in users) {
             if (user.username == inputUsername) {
                 val inputPassword = findViewById<TextInputEditText>(R.id.loginPasswordEditTextId).text.toString()
-                val storedPassword = user.password
-
-                return isCorrectPassword(inputPassword, storedPassword)
+                return BCrypt.checkpw(inputPassword, user.password) // check if password is valid
             }
         }
 
         return false
     }
-
-    private fun isCorrectPassword(pass1: String, pass2: String): Boolean {
-        return false
-    }
-
-
 
 }
