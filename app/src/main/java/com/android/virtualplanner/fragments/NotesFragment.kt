aa47c2899ce8948957e73747a115b54b2bec9831
FER\la51450 <abramovic.laura@gmail.com>
2021-06-10 21:09:17 +0200

    Added popup dialog for to do list

--- app/src/main/java/com/android/virtualplanner/fragments/NotesFragment.kt
+++ app/src/main/java/com/android/virtualplanner/fragments/NotesFragment.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.android.virtualplanner.R
+import com.android.virtualplanner.dao.UserDao
+import com.android.virtualplanner.database.UserDatabase
 import com.android.virtualplanner.preferences.AppPreferences
 
 class NotesFragment : Fragment() {
+    private lateinit var dao: UserDao
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val username = AppPreferences.username
+
+        var root = inflater.inflate(R.layout.fragment_notes, container, false)
 
         Toast.makeText(activity, "From notes: $username", Toast.LENGTH_LONG).show()
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false)
+        return root
+
+
     }
 }
+
--- app/src/main/java/com/android/virtualplanner/fragments/TodoFragment.kt
+++ app/src/main/java/com/android/virtualplanner/fragments/TodoFragment.kt
@@ -1,20 +1,41 @@
 package com.android.virtualplanner.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.android.virtualplanner.R
 import com.android.virtualplanner.preferences.AppPreferences
 
 class TodoFragment : Fragment() {
+    private lateinit var imageAddButton: ImageButton
+    private lateinit var dialogBuilder: AlertDialog.Builder
+    private lateinit var dialog: AlertDialog
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val username = AppPreferences.username
 
-        Toast.makeText(activity, "From todo: $username", Toast.LENGTH_LONG).show()
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo, container, false)
+        var root = inflater.inflate(R.layout.fragment_todo, container, false)
+        imageAddButton = root.findViewById(R.id.todoAddImageButtonId)
+
+        imageAddButton.setOnClickListener {
+            createPopupDialog()
+        }
+
+        return root
+    }
+
+    private fun createPopupDialog() {
+        val view = layoutInflater.inflate(R.layout.popup_add_todo_item, null)
+        dialogBuilder = AlertDialog.Builder(activity)
+
+        dialogBuilder.setView(view)
+        dialog = dialogBuilder.create()
+        dialog.show()
+
     }
 }
